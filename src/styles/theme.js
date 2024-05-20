068566d696acfbc894dd325bcd55a2e157b8d084
kwon Jaeeun <58484758+Jaeeun98@users.noreply.github.com>
2024-05-21 07:56:39 +0900

    css 세팅

---
+++ src/styles/theme.js
@@ -0,0 +1,11 @@
+const theme = {
+  colors: {
+    point: "#521E95",
+    default: "#F9F6FD",
+    text_default: "#BA95EA",
+    gray: "#696969",
+    delete: "#FF5000",
+  },
+};
+
+export default theme;
