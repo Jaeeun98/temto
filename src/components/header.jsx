3411804f99f98cd3f1b47ed939a4323f851f25ea
kwon Jaeeun <58484758+Jaeeun98@users.noreply.github.com>
2024-08-19 22:49:16 +0900

    header 컴포넌트 추가

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,25 @@
+import styled from "styled-components";
+import theme from "../styles/theme";
+
 export default function Header() {
   return (
-    <header>{/* <img src="/images/logo.png" alt="temto_logo" /> */}</header>
+    <HeaderStyle>
+      <img src="/images/logo.png" alt="temto_logo" />
+    </HeaderStyle>
   );
 }
+
+const HeaderStyle = styled.header`
+  background-color  : ${theme.colors.default};
+  border-bottom: ${theme.colors.line_gray};
+  height: 80px;
+  padding-left: 20px;
+  display: flex;
+  align-items: center;
+
+
+  img {
+    width: 70px;
+    height: 40px;
+  }
+`
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -2,9 +2,10 @@ const theme = {
   colors: {
     point: "#8D50DC",
     default: "#F9F6FD",
+    line_gray: "#DDDDDD",
     text_default: "#BA95EA",
     gray: "#F6F6F6",
-    text_gray: "#979797",
+    text_gray: "#3A3A3A",
     delete: "#FF5000",
   },
 };
