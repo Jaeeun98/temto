3f670d41984c7951a78274d7b35deb05fe5120a0
kwon Jaeeun <58484758+Jaeeun98@users.noreply.github.com>
2024-09-12 01:48:48 +0900

    굿즈 api 추가

---
+++ src/types/goods.ts
@@ -0,0 +1,11 @@
+interface GoodsDetail {
+  goodsName: string;
+  goodsPrice: string;
+  goodsColor: string;
+  goodsSize: string;
+  goodsFrom: string;
+  goodsReleaseDate: string;
+  goodsDeliveryDate: string;
+  areaCodeId: string;
+  detailAreaCodeId: string;
+}
