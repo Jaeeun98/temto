684573637ccaadbea9b560b6d2e61be0bd2dd373
kwon Jaeeun <58484758+Jaeeun98@users.noreply.github.com>
2024-09-11 22:44:19 +0900

    입금확인 버튼 ui 추가

--- src/api/order.ts
+++ src/api/order.ts
@@ -2,6 +2,6 @@ import { axiosInstance } from "./axiosClient";
 
 //주문 목록 리스트
 export const getOrderList = async (page: number) => {
-  const result = await axiosInstance.get(`/goods/list?page=${page}`);
+  const result = await axiosInstance.get(`/order-item/list?page=${page}`);
   return result?.data;
 };
