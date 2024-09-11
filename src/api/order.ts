b2219a11c389a515faf6d423325a0c8c64b20946
kwon Jaeeun <58484758+Jaeeun98@users.noreply.github.com>
2024-09-12 01:42:17 +0900

    주문 api 추가

--- src/api/order.ts
+++ src/api/order.ts
@@ -5,3 +5,17 @@ export const getOrderList = async (page: number) => {
   const result = await axiosInstance.get(`/order-item/list?page=${page}`);
   return result?.data;
 };
+
+//주문 상태 리스트
+export const getOrderStatusList = async () => {
+  const result = await axiosInstance.get(`/order-item/status`);
+  return result?.data;
+};
+
+//주문 상태 변경
+export const orderStatusChange = async (status: string) => {
+  const result = await axiosInstance.put(
+    `/order-item/status?orderId=${status}`
+  );
+  return result?.data;
+};
