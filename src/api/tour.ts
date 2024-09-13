ac49decf7fd79c864c36af8b3bc73269f9fd9576
kwon Jaeeun <58484758+Jaeeun98@users.noreply.github.com>
2024-09-13 10:37:15 +0900

    관광지 리스트 api 연결

---
+++ src/api/tour.ts
@@ -0,0 +1,7 @@
+import { axiosInstance } from "./axiosClient";
+
+//관광지 목록 리스트
+export const getTorismList = async (page: number) => {
+  const result = await axiosInstance.get(`/toureism/list?page=${page}`);
+  return result?.data;
+};
--- src/pages/tour.tsx
+++ src/pages/tour.tsx
@@ -1,11 +1,54 @@
 import React from "react";
 import ListContainer from "../components/common/list_container";
 import { TableContainer } from "../styles/table_container";
+import useFetchAndSetTableData from "../hooks/useSetTableData";
+import { getTorismList } from "../api/tour";
 
+const columns = [
+  {
+    Header: "",
+    accessor: "checkbox",
+  },
+  {
+    Header: "관광지 ID",
+    accessor: "tourPlaceId",
+  },
+  {
+    Header: "관광지명",
+    accessor: "tourPlaceName",
+  },
+  {
+    Header: "주소",
+    accessor: "tourPlaceAddress",
+  },
+  {
+    Header: "링크",
+    accessor: "tourPlaceLink",
+  },
+  {
+    Header: "연락처",
+    accessor: "tourPlaceContact",
+  },
+  {
+    Header: "수정",
+    accessor: "orderStatus",
+  },
+];
+
+//관광지 리스트
 export default function Tour() {
+  useFetchAndSetTableData(
+    ["tourList", 0],
+    () => getTorismList(1),
+    true, // 버튼 추가 여부를 결정하는 매개변수
+    columns
+  );
+
+  console.log("tour");
+
   return (
     <TableContainer>
-      <ListContainer title={"관광지 리스트"} />
+      <ListContainer />
     </TableContainer>
   );
 }
